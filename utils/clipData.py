a6a8cdcf054e29564f6dca475f7497f66483d132
KasperFrahm <gjr449@alumni.ku.dk>
2024-04-30 13:40:13 +0200

    modified quantileSelection in clipData.py and tested it.

--- utils/clipData.py
+++ utils/clipData.py
@@ -16,19 +16,20 @@ def noisyCount(D, start, end):
     right_idx = np.searchsorted(D, end, side='right')
     count = right_idx - left_idx
     # Unsure of we are adding the correct noise
-    privacy_budget = 0.5
-    scale = np.log(np.max(D)) / (2 * privacy_budget)
-    noise = np.random.normal(0, scale)
+    #privacy_budget = 0.5
+    #scale = np.log(np.max(D)) / (2 * privacy_budget)
+    #noise = np.random.normal(0, scale)
+    noise = laplace_mechanism(1, 1)
     return count + noise
 
 # Threshold is decided privately
-def quantileSelection(D):
+def quantileSelection(D, m):
     left = np.min(D)
     right = np.max(D)
-    m = 0.999 * np.size(D)
     while left < right:
         mid = np.floor((left + right) / 2)
         c = noisyCount(D, np.min(D), mid)
+        print(c)
         if c < m:
             left = mid + 1
         else:
@@ -48,7 +49,7 @@ def clipData(dataset, thresh):
 def clip(df, column):
     df_column = df[column]
     df_cons_sorted = np.sort(df_column)
-    thresh = quantileSelection(df_cons_sorted)
+    thresh = quantileSelection(df_cons_sorted, 0.999 * np.size(df_cons_sorted))
     #print(thresh)
     clippedData = clipData(df_column, thresh)
     return clippedData, thresh
@@ -60,3 +61,7 @@ def clip_pr_column(df):
         if column != 'HourDK':
             df[column], thresh = clip(df, column)
     return df
+
+data = [21, 123, 213, 276, 282, 323, 374, 424, 488, 523, 576, 628, 698, 734, 784, 1239, 1419, 12302, 102329]
+
+print(quantileSelection(data, 0.999 * np.size(data)))
