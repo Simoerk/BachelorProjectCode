e789a8d8acbf834ec7c24b18a499382407f6bf7a
KasperFrahm <gjr449@alumni.ku.dk>
2024-05-13 12:21:32 +0200

    compareStatistics

--- experiments/standardDeviation.py
+++ experiments/standardDeviation.py
@@ -6,11 +6,14 @@ import numpy as np
 actual_df = pd.read_csv('results/regional_consumption_sums.csv')
 result_df = pd.read_csv('results/Num2DUnbGeoLoc_noisy_result.csv')
 
-# Extract relevant columns (municipality numbers)
-municipalities_result = result_df.columns[1:]  # Exclude first column (HourDK)
+# Exclude first column (HourDK) in actual_df
+actual_df = actual_df.drop(actual_df.columns[0], axis=1)
 
-# Reorder columns of df2 to match df1
-actual_df = actual_df.reindex(columns=municipalities_result)
+# Exclude first column (HourDK) in result_df
+result_df = result_df.drop(result_df.columns[0], axis=1)
+
+# Reorder columns of actual_df to match result_df
+actual_df = actual_df[result_df.columns]
 
 # Make a copy of the DataFrames
 diff_df_copy = actual_df.copy()
@@ -30,12 +33,14 @@ def avg_discrepancy(actual_df, result_df):
     for i in actual_df.columns:
         for index, row in actual_df.iterrows():
             diff_df_copy.at[index, i] = abs(actual_df.at[index, i] - result_df.at[index, i])
-    # Calculate the average of the differnces in std_df_copy
+    # Calculate the average of the differnces in dff_df_copy
     avg = diff_df_copy.mean(axis=0)
     return avg
                     
 # Calculate the average discrepancy
 avg_dis = avg_discrepancy(actual_df, result_df)
+
+print("avg_dis: ", avg_dis)
 
 # Calculate the standard deviation of the differences
 std_dev = std_deviation(actual_df, result_df)
