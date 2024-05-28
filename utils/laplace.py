ff35f379aab6dded4aa693c5a28c6a13cf843942
Simon Mørk <jvl527@alumni.ku.dk>
2024-05-28 14:36:39 +0200

    laplace fix

--- utils/laplace.py
+++ utils/laplace.py
@@ -3,7 +3,7 @@ import pandas as pd
 import math
 
 # Function to apply the Laplace mechanism along with the value for differential privacy 
-def laplace_mechanism(value, sensitivity, epsiondenom, epsilon):
+def laplace_mechanism(value, sensitivity, epsilon):
     # The sensitivity of a sum query is 1
     scale = sensitivity / epsilon
     # Generate Laplace noise
