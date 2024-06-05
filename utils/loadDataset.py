2d0381214de150d8d767dbdf45777bbd701055fa
Simon Mørk <jvl527@alumni.ku.dk>
2024-06-05 13:39:06 +0200

    Update loadDataset.py

--- utils/loadDataset.py
+++ utils/loadDataset.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 # Simple function to load the dataset
-def load_dataset(str, n): 
-    print("Loading the big dataset...")
+def load_dataset(str, n):
     data = pd.read_csv(str, nrows=n)
-    print("Dataset loaded successfully!")
     return data
